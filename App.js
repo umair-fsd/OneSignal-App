69394fe149d2fa35bce50725e6294bb4ea8490d0
umair.fsd <umair.arshad06@gmail.com>
2021-04-17 17:27:41 +0500

    Added OneSignal Push Notifications

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import OneSignal from "react-native-onesignal";
+import { Text, View, StyleSheet, StatusBar, Linking } from "react-native";
 
 export default function App() {
+  /////ONE SIGNAL IMPLEMENTATION/////
+  const onReceived = (notification) => {
+    console.log("Notification received: ", notification);
+  };
+
+  const onOpened = async (openResult) => {
+    console.log("Message: ", openResult.notification.payload.body);
+    console.log("isActive: ", openResult.notification.isAppInFocus);
+    console.log("openResult: ", openResult);
+    console.log("Data: ", openResult.notification.payload.additionalData);
+  };
+
+  const onIds = (device) => {
+    console.log("Device info: ", device);
+  };
+  useEffect(() => {
+    // Setting requestPermissions
+    const permissions = {
+      alert: true,
+      badge: true,
+      sound: true,
+    };
+    OneSignal.requestPermissions(permissions);
+    OneSignal.setLogLevel(6, 0);
+
+    OneSignal.init("YOUR_APP_ID", {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+    OneSignal.addEventListener("received", onReceived);
+    OneSignal.addEventListener("opened", onOpened);
+    OneSignal.addEventListener("ids", onIds);
+
+    return () => {
+      OneSignal.removeEventListener("received", onReceived);
+      OneSignal.removeEventListener("opened", onOpened);
+      OneSignal.removeEventListener("ids", onIds);
+    };
+  }, []);
+
+  ////////
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello React Native</Text>
     </View>
   );
 }
@@ -14,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
